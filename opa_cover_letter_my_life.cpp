b1e328df21d4319af98ae5d2bea96076970c7a55
Pavel <mobelus@mail.ru>
2017-02-27 11:21:45 +0300

    Update opa_cover_letter_my_life.cpp

--- opa_cover_letter_my_life.cpp
+++ opa_cover_letter_my_life.cpp
@@ -1,6 +1,20 @@
 Меня зовут ОПА. Мне 30 лет. На данный момент я работаю программистом в страховой компании, в которой я занимаюсь разработкой 
 фронт-офисной части АРМа документооборота внутренних договоров компании.
 В 2004 году я поступил в МИЭМ. Московский институт электроники и математики на Факультет прикладной математики, специальность Компьютерная Безопасности.
-На 2 курсе институа я начал работать Системным администратором, чтобы начать получать первый опыт работы в IT-индустрии.
+Im ersten Studiengang hatten wir Pascal. Danach im 2-ten Studiengang C++. Ab den 3-tten hatten wir leider keine Programmiersprachen mehr
+aber wir hatten OperationsSysteme, und ich bin deswegen auf extrakurse gegangen - C, C++ Kurse also, weil die Laborarbeiten beno:togten
+etwas extra mehr wissen, als das was uns beigebracht war.
+Zuru:ck zur Arbeit. Im 2 2 курсе институа я начал работать Системным администратором, чтобы начать получать первый опыт работы в IT-индустрии.
 Я параллельно работал и учился и потому работал лишь на пол ставки, чтобы была возможность совмещать работу и учёбу.
 В конце 4 курса нам пре
+
+Ich heisse Obrosov Pavel Alexandrovich. Ich bin 30 Jahre alt. Zur Zeit arbeite ich als C++ Softwareentwickler in der Versicherungsgesellschaft
+, in der ich das Front-Ofice der APS (ArbeitsPlatzStation) fur Creation und Steuerung vom Dokumentenumlauf der Vertra:gen der Firma.
+В 2004 году я поступил в МИЭМ. Московский институт электроники и математики на Факультет прикладной математики, специальность Компьютерная Безопасности.
+Im ersten Studiengang hatten wir Pascal. Danach im 2-ten Studiengang C++. Ab den 3-tten hatten wir leider keine Programmiersprachen mehr
+aber wir hatten OperationsSysteme, und ich bin deswegen auf extrakurse gegangen - C, C++ Kurse also, weil die Laborarbeiten beno:togten
+etwas extra mehr wissen, als das was uns beigebracht war.
+Zuru:ck zur Arbeit. Im 2 2 курсе институа я начал работать Системным администратором, чтобы начать получать первый опыт работы в IT-индустрии.
+Я параллельно работал и учился и потому работал лишь на пол ставки, чтобы была возможность совмещать работу и учёбу.
+В конце 4 курса нам пре
+
